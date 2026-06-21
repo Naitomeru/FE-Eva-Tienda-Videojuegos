07851e79d4cec641bcd932f5f8e00dde51f7a801
Naitomeru <ronnaitomeru@gmail.com>
2026-06-20 22:47:45 -0400

    Finished migrating to React

--- test.jsx
+++
@@ -1,5 +0,0 @@
-import { createRoot } from 'react-dom/client';
-
-// Render your React component instead
-const root = createRoot(document.getElementById('index-main'));
-root.render(<h1>Hello, world</h1>);
