92dccac57df0730d276240fd75681877bd5d3272
Naitomeru <ronnaitomeru@gmail.com>
2026-06-22 17:04:22 -0400

    Fixed carousel images and Details page style

--- src/components/BrandCarousel.jsx
+++ src/components/BrandCarousel.jsx
@@ -1,55 +1,58 @@
 import { Link } from 'react-router-dom'
+import NintendoLogo from '../assets/logos/nintendo.png'
+import PlayStationLogo from '../assets/logos/playstation.png'
+import XboxLogo from '../assets/logos/xbox.png'
 
 export function BrandCarousel() {
     return (
         <section className="brands-carousel">
             <div className="brands-track">
                 <Link to="/nintendo" className="brand-item">
-                    <img src="images/logos/nintendo.png" alt="Nintendo" />
+                    <img src={NintendoLogo} alt="Nintendo" />
                 </Link>
 
                 <Link to="/playstation" className="brand-item">
-                    <img src="images/logos/playstation.png" alt="PlayStation" />
+                    <img src={PlayStationLogo} alt="PlayStation" />
                 </Link>
 
                 <Link to="/xbox" className="brand-item">
-                    <img src="images/logos/xbox.png" alt="Xbox" />
+                    <img src={XboxLogo} alt="Xbox" />
                 </Link>
 
                 <Link to="/nintendo" className="brand-item">
-                    <img src="images/logos/nintendo.png" alt="Nintendo" />
+                    <img src={NintendoLogo} alt="Nintendo" />
                 </Link>
 
                 <Link to="/playstation" className="brand-item">
-                    <img src="images/logos/playstation.png" alt="PlayStation" />
+                    <img src={PlayStationLogo} alt="PlayStation" />
                 </Link>
 
                 <Link to="/xbox" className="brand-item">
-                    <img src="images/logos/xbox.png" alt="Xbox" />
+                    <img src={XboxLogo} alt="Xbox" />
                 </Link>
 
                 <Link to="/nintendo" className="brand-item">
-                    <img src="images/logos/nintendo.png" alt="Nintendo" />
+                    <img src={NintendoLogo} alt="Nintendo" />
                 </Link>
 
                 <Link to="/playstation" className="brand-item">
-                    <img src="images/logos/playstation.png" alt="PlayStation" />
+                    <img src={PlayStationLogo} alt="PlayStation" />
                 </Link>
 
                 <Link to="/xbox" className="brand-item">
-                    <img src="images/logos/xbox.png" alt="Xbox" />
+                    <img src={XboxLogo} alt="Xbox" />
                 </Link>
 
                 <Link to="/nintendo" className="brand-item">
-                    <img src="images/logos/nintendo.png" alt="Nintendo" />
+                    <img src={NintendoLogo} alt="Nintendo" />
                 </Link>
 
                 <Link to="/playstation" className="brand-item">
-                    <img src="images/logos/playstation.png" alt="PlayStation" />
+                    <img src={PlayStationLogo} alt="PlayStation" />
                 </Link>
 
                 <Link to="/xbox" className="brand-item">
-                    <img src="images/logos/xbox.png" alt="Xbox" />
+                    <img src={XboxLogo} alt="Xbox" />
                 </Link>
             </div>
         </section>
